9229121395d74876a03945f2db77f5505f637050
odex14 <lanachelidze92@gmail.com>
2024-05-30 19:04:53 +0400

    goa

---
+++ level_2/homework.py
@@ -0,0 +1,11 @@
+print("aleqsandre odikadze")
+
+age = 9
+name = "aleqsandre odikadze"
+print(age)
+print(name)
+
+
+
+
+
---
+++ level_2/level3.py/classwork.py
@@ -0,0 +1,6 @@
+print("aleqsandre odikadze")
+
+age = 9
+
+print(age)
+print(age)
