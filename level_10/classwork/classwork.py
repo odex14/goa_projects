65a4c41a71720236c9f8159336bcecb2825d2b6a
odex14 <lanachelidze92@gmail.com>
2024-07-19 19:56:05 +0400

    Goa

---
+++ level_10/classwork/classwork.py
@@ -0,0 +1,8 @@
+full_name = input("whats your full_name? :")
+lemur_food = input("what food do you like? :")
+
+print("Hi,"+ full_name + " what food does the lemur like? " + lemur_food )
+
+
+
+
---
+++ level_10/homework/homework.py
@@ -0,0 +1,9 @@
+1
+name = input("Whats your name?")
+surname = input("Whats your surname?")
+print(name + " " + surname)
+
+2
+full_name = input("whats your fullname?")
+ 
+print(full_name + " " + "Happy BirthDay I Wish You a Great Day With Your Family")
