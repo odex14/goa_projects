ac0e517f233afb58730fa6e5736bdaf4e483f20e
odex14 <lanachelidze92@gmail.com>
2024-07-28 12:28:29 +0400

    Goa

--- level_10/homework/homework.py
+++ level_10/homework/homework.py
@@ -7,3 +7,21 @@ print(name + " " + surname)
 full_name = input("whats your fullname?")
  
 print(full_name + " " + "Happy BirthDay I Wish You a Great Day With Your Family")
+
+3
+Animal = input("Whats your favorite animal")
+color = input("Whats your favorite color")
+car = input("Whats your favorite car")
+actor = input("Whos your favorite actor")
+print("My Favorite animal is" + " " + Animal + " " + "My Favorite color is" + " " + color + " " + "My Favorite car is"+ " " + car + " " +"My Favorite actor is" + " " + actor )
+4
+#Inputs&Outputs
+#In computing, input/output (I/O, i/o, or informally io or IO) is the communication between an information processing 
+#system, such as a computer, and the outside world, such as another computer system, peripherals, or a human operator.
+# Inputs are the signals or data received by the system and outputs are the signals or data sent from it.
+# The term can also be used as part of an action; to "perform I/O" is to perform an input or output operation.
+5
+#Connection
+#In formal language theory and computer programming, string concatenation is the operation of joining character strings end-to-end.
+# For example, the concatenation of "snow" and "ball" is "snowball".
+# In certain formalisations of concatenation theory, also called string theory, string concatenation is a primitive notion.
