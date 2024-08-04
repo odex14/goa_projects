e4645e883b17858a962f100bbfb6f801f13fcbee
odex14 <lanachelidze92@gmail.com>
2024-08-04 13:29:53 +0400

    Goa

---
+++ level_12/homework/homework.py
@@ -0,0 +1,37 @@
+2
+#String,str is something thats has "" Quotation marks like "Hello world"
+#Integer,int is a number, a whole number like 10 or -56 
+#Float is a number that is not whole like 4.5 or 32.96
+
+3
+#To do so, we use the following Pandas method: df.info () Using info gives 
+#us a breakdown of all of the columns in our DataFrame, how many non-null values each column contains, and the DataTypes.
+#We verify that there are indeed text columns in this dataset, which in Pandas fall under the “object” data type.
+
+4
+print("he" + "llo" )
+print("ha" + "ppy" )
+print("pa" + "nda" )
+print("dra" + "gon" )
+print("ho" + "rse" )
+print("par" + "ty" )
+print("fa" + "ther" )
+print("off" + "ice" )
+print("sta" + "dy" )
+print("da" + "ce" )
+print("geo" + "rgia" )
+print("mo" + "ther" )
+print("mo" + "ney" )
+print("bath" + "room" )
+print("wa" + "ter" )
+print("po" + "tato" )
+print("to" + "mato" )
+print("sa" + "use" )
+print("car" + "rot" )
+print("app" + "le" )
+
+
+5
+full_name = input("whats your fullname?")
+favorite_food = input("whats your favorite food?")
+print(full_name + " " + favorite_food)
