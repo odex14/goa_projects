deeda5f93b3ec3381fd7c443f93777019d917232
odex14 <lanachelidze92@gmail.com>
2024-08-25 21:31:22 +0400

    Goa

---
+++ level_16/classwork/classwork.py
@@ -0,0 +1,34 @@
+a = 5
+b = 10
+c = 7.5
+d = 15
+
+#<
+print(a < b)
+print(b < c)
+print(c < d)
+print(d < d)
+#>
+print(a > c)
+print(c > d)
+print(b > a)
+print(b < d)
+#<=
+print(d <= a)
+print(c <= c)
+print(b <= a)
+print(c < 6)
+#>=
+print(a >= b)
+print(d >= d)
+print(b >= c)
+print(7 < d)
+#!=
+print(a != c)
+print(b != d)
+print(b != b)
+print(14 < d)
+#==
+print(b == c)
+print(b == b)
+print(a == d)
---
+++ level_16/homework/homework.py
@@ -0,0 +1,47 @@
+#In the field of computers and electronics,
+#Boolean refers to a data type that has two possible values representing true and false.
+
+
+#homework_4
+a = 5
+b = 10
+c = 7.5
+d = 15
+
+#<
+print(a < b)
+print(b < c)
+print(c < d)
+print(a < a)
+print(d < d)
+#>
+print(a > c)
+print(c > d)
+print(b > a)
+print(b < d)
+print(d < d)
+#<=
+print(d <= a)
+print(c <= c)
+print(b <= a)
+print(c < 6)
+print(d < d)
+#>=
+print(a >= b)
+print(d >= d)
+print(b >= c)
+print(7 < d)
+print(d < d)
+#!=
+print(a != c)
+print(b != d)
+print(b != b)
+print(14 < d)
+print(d < d)
+#==
+print(b == c)
+print(b == b)
+print(a == d)
+print(b < d)
+print(d < d)
+
