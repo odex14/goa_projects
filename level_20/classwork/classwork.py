ece461a0a3f4d27c8ed50126616b046ac3c551b3
odex14 <lanachelidze92@gmail.com>
2024-10-06 17:20:37 +0400

    GOA

---
+++ level_20/classwork/classwork.py
@@ -0,0 +1,20 @@
+#1
+first=input("first number")
+second=input("first second")
+
+print(first == second)
+
+#2
+
+first=input("moms age")
+second=input("dads age")
+
+print(first > second)
+
+#3
+
+
+#4
+num2=0
+first = int(input('number'))
+print(first >= num2)
