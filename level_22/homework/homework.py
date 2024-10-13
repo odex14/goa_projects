dcac2032a63d3865a922b5ecc5d019b8857762ea
odex14 <lanachelidze92@gmail.com>
2024-10-13 15:00:00 +0400

    Goa

---
+++ level_22/homework/homework.py
@@ -0,0 +1,49 @@
+#1
+num=input(("pick a number"))
+a=10
+print(a < (int (num)))
+
+
+
+
+
+#2
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+  print("{0} is true Evenn".format(num))
+else:
+    print("{0} is false Odd".format(num))
+
+
+
+
+
+#3
+
+y=input("a number")
+
+print((int(y)) > 50)
+print((int(y)) < 100)
+
+
+#4
+z=input("first number")
+s=input("second number")
+d=input("Third number")
+
+print((int(z) == (int(s))) == (int(d)))
+
+#5
+
+name="aleksandre"
+pasword="20140"
+
+n=input("your name")
+p=input("your pasword")
+
+print(name == n)
+print(pasword == p)
