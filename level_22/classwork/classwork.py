a03617a3e184c885a6fde476d89987d46c446fb7
odex14 <lanachelidze92@gmail.com>
2024-10-20 17:50:17 +0400

    goa

---
+++ level_22/classwork/classwork.py
@@ -0,0 +1,34 @@
+for i in range(10):
+    print("Aleksandre")
+
+
+for i in range(20):
+    print("GOA best")
+
+
+
+for i in range(30):
+    print("helo world")
+
+
+
+
+for i in range(50):
+    print(i)
+
+
+
+
+
+for g in range(30):
+    print(g)
+
+
+
+
+for a in range(30):
+    print(a + 5)
+
+
+for i in range(10,51):
+    print(i * 2)
